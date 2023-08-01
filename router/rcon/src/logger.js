ae75a495e23d69c63cf695362a60965cdc18b97b
Zer0Power <zeroopoower@gmail.com>
2023-08-01 16:51:24 +0330

    feat: rcon logger

---
+++ router/rcon/src/logger.js
@@ -0,0 +1,21 @@
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+//Packages
+
+const fs = require('fs');
+const time = require('node-get-time');
+const util = require('util');
+const path = require('path')
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+//Logger Function
+
+const logPath = path.join(__dirname, "..", "/logs/", `${process.pid}-debug.log`);
+
+let log_file = fs.createWriteStream(logPath, { flags: 'w' });
+
+module.exports = async (d) => {
+
+    log_file.write("[" + time.now() + "] " + util.format(d) + '\n');
+
+}
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
