738a6d024163eaf04a90da9be878b714001ee9af
Zer0Power <zeroopoower@gmail.com>
2023-08-01 17:33:40 +0330

    fix: faceit player data

--- router/faceit/routes/player.js
+++ router/faceit/routes/player.js
@@ -1,38 +1,40 @@
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
-//Packages 
+//Packages
 
 const express = require("express");
 const router = express.Router();
 
-const getData = require('../src/getData');
+const getData = require("../src/getData");
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
-//Router 
+//Router
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  gameplayerid = req.query.id;
 
-    gameplayerid = req.query.id
-
-    if (gameplayerid.length == 17) {
-
-        try {
-            const data = await getData(gameplayerid)
-            if (data) res.status(200).send({ status: 200, data })
-
-        } catch (error) {
-
-            return res.status(408).json({ status: 408, error: error, message: "api is currently down" });
-        }
-
+  if (gameplayerid.length == 17) {
+    try {
+      const data = await getData(gameplayerid);
+      if (data.status === 200)
+        res.status(200).send({ status: 200, data: data.data });
+      else {
+        return res.status(404).json({
+          status: 404,
+          error: "the requested player does not exist",
+          message: "faceit account not found",
+        });
+      }
+    } catch (error) {
+      return res
+        .status(408)
+        .json({ status: 408, error: error, message: "api is currently down" });
     }
-
-    else {
-        return res.status(400).json({ status: 400, error: "Bad Request", message: "invalid query" });
-    }
-
-
-})
-
+  } else {
+    return res
+      .status(400)
+      .json({ status: 400, error: "Bad Request", message: "invalid query" });
+  }
+});
 
 module.exports = router;
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
