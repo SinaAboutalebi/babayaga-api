7e02d123b97abfc011b261ba39e2e411b25ebd91
Zer0Power <zeroopoower@gmail.com>
2023-08-01 16:30:57 +0330

    feat: pretty , Server StartUp

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
 //Packages
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const cors = require("cors");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 
-const router = require('./router');
+const router = require("./router");
 
 const app = express();
 
@@ -21,21 +21,40 @@ cyan = "\x1b[36m";
 blue = "\x1b[34m";
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
-//Access Log Stream 
+//Access Log Stream
 
-let date = new Date()
-let formatedDate = date.toISOString().split('T')[0]
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access', `${formatedDate}-access.log`), { flags: 'a', interval: '1d' })
+let date = new Date();
+let formatedDate = date.toISOString().split("T")[0];
+var accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "access", `${formatedDate}-access.log`),
+  { flags: "a", interval: "1d" }
+);
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
-//MiddleWares 
+//MiddleWares
 
-app.use(cors({ methods: ['GET', 'POST'] }));
+app.use(cors({ methods: ["GET", "POST"] }));
 app.use(bodyParser.json()); //Parse JSON bodies
-app.use(bodyParser.urlencoded({extended: true})); //Parse URL-encoded bodies
-app.use(morgan("common")) //Console Logger 
-app.use(morgan("common", { stream: accessLogStream })) //Access File Log
-app.use(router)
+app.use(bodyParser.urlencoded({ extended: true })); //Parse URL-encoded bodies
+app.use(morgan("common")); //Console Logger
+app.use(morgan("common", { stream: accessLogStream })); //Access File Log
+app.use(router);
 app.use("*", (req, res) => {
-    res.status(401).send({ status: 401, error: 'Unauthorized', message: "client failed to authenticate with the server" });
+  res
+    .status(401)
+    .send({
+      status: 401,
+      error: "Unauthorized",
+      message: "client failed to authenticate with the server",
+    });
 });
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+//Server StartUP
+
+app.listen(process.env.PORT, async () => {
+  console.log(magenta, "[📶]Server Is Running Properly ....");
+  console.log(cyan, " [⚙️]Port : ", process.env.PORT);
+});
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
